e210a908f38c9fec66fef2fcc796164b703a66ab
proshunsuke <shunsuke0901@gmail.com>
2016-01-04 00:41:45 +0900

    ボタン適用

--- web/static/js/top/components/Home.js
+++ web/static/js/top/components/Home.js
@@ -1,14 +1,15 @@
 const React = require('react');
 const { connect } = require('react-redux');
 const { increase, decrease } = require('../actions/count');
+import { Button } from 'react-bootstrap';
 
 function Home({ number, increase, decrease }) {
   return (
     <div>
       Some state changes:
       {number}
-      <button onClick={() => increase(1)}>Increase</button>
-      <button onClick={() => decrease(1)}>Decrease</button>
+      <Button onClick={() => increase(1)}>Increase</Button>
+      <Button onClick={() => decrease(1)}>Decrease</Button>
     </div>
   );
 };
