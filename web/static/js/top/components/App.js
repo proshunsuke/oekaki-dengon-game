9c8144c65c532a4c21fc9db8bd6c6e4096d29817
proshunsuke <shunsuke0901@gmail.com>
2016-01-04 02:54:54 +0900

    入力フォーム

--- web/static/js/top/components/App.js
+++ web/static/js/top/components/App.js
@@ -6,38 +6,35 @@ import { Button, Navbar, NavItem, MenuItem, NavDropdown, Nav } from 'react-boots
 import { LinkContainer } from 'react-router-bootstrap';
 
 function App({ pushPath, children }) {
-  return (
-    <div>
-      <header>
-          <Navbar>
-              <Navbar.Header>
-                  <Navbar.Brand>
-                      <Link to="/">お絵かき伝言ゲーム</Link>
-                  </Navbar.Brand>
-              </Navbar.Header>
-              <Nav>
-                  <LinkContainer to="/"><NavItem>Home</NavItem></LinkContainer>
-                  <LinkContainer to="/foo"><NavItem>Foo</NavItem></LinkContainer>
-                  <LinkContainer to="/bar"><NavItem>Boo</NavItem></LinkContainer>
-                   <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                      <MenuItem eventKey={3.1}>Action</MenuItem>
-                      <MenuItem eventKey={3.2}>Another action</MenuItem>
-                      <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                      <MenuItem divider />
-                      <MenuItem eventKey={3.3}>Separated link</MenuItem>
-                  </NavDropdown>
-              </Nav>
-          </Navbar>
-      </header>
-      <div>
-        <Button onClick={() => pushPath('/foo')}>Go to /foo</Button>
-      </div>
-      <div style={{marginTop: '1.5em'}}>{children}</div>
-    </div>
-  );
+    return (
+        <div>
+            <header>
+                <Navbar>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <Link to="/">お絵かき伝言ゲーム</Link>
+                        </Navbar.Brand>
+                    </Navbar.Header>
+                    <Nav>
+                        <LinkContainer to="/"><NavItem>Home</NavItem></LinkContainer>
+                        <LinkContainer to="/foo"><NavItem>Foo</NavItem></LinkContainer>
+                        <LinkContainer to="/bar"><NavItem>Boo</NavItem></LinkContainer>
+                        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                            <MenuItem eventKey={3.1}>Action</MenuItem>
+                            <MenuItem eventKey={3.2}>Another action</MenuItem>
+                            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                            <MenuItem divider/>
+                            <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar>
+            </header>
+            <div style={{marginTop: '1.5em'}}>{children}</div>
+        </div>
+    );
 };
 
 module.exports = connect(
-  null,
-  { pushPath }
+    null,
+    {pushPath}
 )(App);
--- web/static/js/top/components/CreateRoom.js
+++ web/static/js/top/components/CreateRoom.js
@@ -1,10 +1,12 @@
 const React = require('react');
-import { Button } from 'react-bootstrap';
+import { Button, Input, ButtonInput } from 'react-bootstrap';
 
 module.exports = function CreateRoom() {
     return <div>
-        <p>ここに部屋の名前</p>
-        <p>ここにパスワード</p>
-        <Button>部屋を作る</Button>
+        <form>
+            <Input type="text" label="部屋の名前" placeholder="部屋の名前を入力してください" />
+            <Input type="password" label="パスワード" placeholder="パスワードを入力してください" />
+            <ButtonInput type="submit" value="送信" />
+        </form>
     </div>;
 }
