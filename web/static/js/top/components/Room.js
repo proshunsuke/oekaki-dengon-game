181f3145e702f2cd9a238ff64f1cc66229a395f7
proshunsuke <shunsuke0901@gmail.com>
2016-02-02 09:52:31 +0900

    canvas使ってみる

--- web/static/js/top/components/Room.js
+++ web/static/js/top/components/Room.js
@@ -10,11 +10,23 @@ class Room extends React.Component {
     componentDidMount() {
         const { dispatch } = this.props;
         dispatch(joinRoom());
+        const context = React.getDOMNode(this).getContext('2d');
+        this.paint(context);
+    }
+
+    paint(context) {
+        context.save();
+        context.translate(100, 100);
+        context.rotate(this.props.rotation, 100, 100);
+        context.fillStyle = '#F00';
+        context.fillRect(-50, -50, 100, 100);
+        context.restore();
     }
 
     render() {
         return <div>
             <p>ここルーム</p>
+            <canvas width={200} height={200} />;
         </div>;
     }
 }
