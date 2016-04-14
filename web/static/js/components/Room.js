00bbf7bd31678a0a75b8bf09460ac1d5b226930a
proshunsuke <shunsuke0901@gmail.com>
2016-04-14 23:59:59 +0900

    drawエリア変えられた

--- web/static/js/components/Room.js
+++ web/static/js/components/Room.js
@@ -7,17 +7,23 @@ const OtherService = require('./OtherService');
 
 class Room extends React.Component {
     render() {
-        const { users, client } = this.props;
+        const { users, client, game } = this.props;
+	let drawOrSettingArea;
+	if (game.isSetting && client.role === 'leader') {
+	    drawOrSettingArea = <Status client={client}/>;
+	} else {
+	    drawOrSettingArea = <Draw />;
+	}
 
         return <div>
 	    <Status client={client}/>
 	    <OtherService client={client}/>
-            <Draw />
+            { drawOrSettingArea }
             <UserList users={users}/>
         </div>;
     }
 }
 
-const mapStateToProps = state => ({users: state.users, client: state.client});
+const mapStateToProps = state => ({users: state.users, client: state.client, game: state.game});
 Room.propTypes = { users: PropTypes.array.isRequired };
 module.exports = connect(mapStateToProps)(Room);
