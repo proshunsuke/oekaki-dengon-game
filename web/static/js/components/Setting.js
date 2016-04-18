1bc2f215174309bcbe8d645b64857fe07a63b206
proshunsuke <shunsuke0901@gmail.com>
2016-04-18 23:59:59 +0900

    settingのstyle

--- web/static/js/components/Setting.js
+++ web/static/js/components/Setting.js
@@ -1,8 +1,17 @@
 import React, { PropTypes } from 'react';
 const { connect } = require('react-redux');
 
+// 2箇所でハードコーディングしてるからやめたほうがよい
+const CANVAS_WIDTH = 600;
+const CANVAS_HEIGHT = 400;
+
 class Setting extends React.Component {
     render() {
+	const settingAreaStyle = {
+            border: '1px solid #000000',
+            width: CANVAS_WIDTH,
+            height: CANVAS_HEIGHT
+        };
         return <div>
 	    ここは設定エリア
 	    </div>;
