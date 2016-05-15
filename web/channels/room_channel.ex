232f4174769ffc123343d28782b2d2a0febc7dbd
proshunsuke <shunsuke0901@gmail.com>
2016-05-15 23:59:59 +0900

    特定のクライアントが分からない

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -87,6 +87,10 @@ defmodule OekakiDengonGame.RoomChannel do
     game = room_with_active_users.games |> List.first
     game_users = room_with_active_users.game_users
     next_game_user = Game.next_game_user(game, game_users)
+
+    # IO.inspect OekakiDengonGame.PubSub.subscribers socket.topic
+    IO.inspect socket.id
+    
     # ここ一般化出来そう
     if is_nil(next_game_user) do
       Game.to_finished_by_room_id(room_id(socket.topic))
@@ -96,7 +100,8 @@ defmodule OekakiDengonGame.RoomChannel do
       OekakiDengonGame.Endpoint.broadcast! "lobby", "game_finished", active_room_objects
       {:reply, :ok, socket}
     else
-      IO.inspect socket
+      
+      
       IO.inspect next_game_user.user_id
       next_order_game = Game.save_as_next_order_game(game, next_game_user)
       next_user_id = game_users |> Enum.find(fn gu -> gu.id == next_order_game.current_game_user_id end) |> Map.get(:user_id)
