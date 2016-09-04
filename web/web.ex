cdcbc436a6d49fe8071bd581e1ba073f4a4ec62b
proshunsuke <shunsuke0901@gmail.com>
2016-09-04 23:59:59 +0900

    ecto更新

--- web/web.ex
+++ web/web.ex
@@ -18,8 +18,9 @@ defmodule OekakiDengonGame.Web do
 
   def model do
     quote do
-      use Ecto.Model
+      use Ecto.Schema
 
+      import Ecto
       import Ecto.Changeset
       import Ecto.Query, only: [from: 1, from: 2]
     end
@@ -30,7 +31,8 @@ defmodule OekakiDengonGame.Web do
       use Phoenix.Controller
 
       alias OekakiDengonGame.Repo
-      import Ecto.Model
+      import Ecto
+      import Ecto.Schema
       import Ecto.Query, only: [from: 1, from: 2]
 
       import OekakiDengonGame.Router.Helpers
@@ -62,7 +64,8 @@ defmodule OekakiDengonGame.Web do
       use Phoenix.Channel
 
       alias OekakiDengonGame.Repo
-      import Ecto.Model
+      import Ecto.Schema
+      import Ecto
       import Ecto.Query, only: [from: 1, from: 2]
     end
   end
